d5bdf7a2a44f24713b0628146409a550370980be
Haur Kang <haur.kang@gmail.com>
2019-05-17 16:27:58 +0100

    first commit

---
+++ src/Pokecard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Pokecard extends Component {
+  render() {
+    return (
+      <div>
+        Pokecard
+      </div>
+    )
+  }
+}
+
+export default Pokecard;
---
+++ src/Pokedex.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Pokedex extends Component {
+  render() {
+    return (
+      <div>
+        Pokedex
+      </div>
+    )
+  }
+}
+
+export default Pokedex;
